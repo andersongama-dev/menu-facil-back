8b036b1911c8c34803ca8999ff8bcdb257fe3924
andersongama-dev <andersongamasilva5@gmail.com>
2026-03-14 21:04:05 -0300

    :construction: feat: AI suggest by user preferences

--- routes/aiRoutes.py
+++ routes/aiRoutes.py
@@ -4,6 +4,9 @@ from services.aiService import ai_suggest_menu
 router = APIRouter()
 
 @router.get("/ai/suggest")
-def suggest(user_text: str = Query(..., description="Texto do usuário sobre prato desejado"),
-            context: str = Query("", description="Histórico da conversa anterior")):
-    return ai_suggest_menu(user_text, context)
+def suggest(
+    user_text: str = Query(..., description="Texto do usuário sobre prato desejado"),
+    user_id: str = Query(..., description="ID do usuário"),
+    context: str = Query("", description="Histórico da conversa anterior")
+):
+    return ai_suggest_menu(user_text, user_id, context)
--- services/aiService.py
+++ services/aiService.py
@@ -4,9 +4,10 @@ from utils.ranking import rank_by_profit
 from utils.upsell import get_upsells
 from utils.ai import llm_select
 from utils.items import fetch_all_menu_items
+from models.ai import AIORM
+from database.connection import SessionLocal
 
-
-def ai_suggest_menu(user_input: str, context=""):
+def ai_suggest_menu(user_input: str, user_id, context=""):
 
     items = fetch_all_menu_items()
 
@@ -14,13 +15,21 @@ def ai_suggest_menu(user_input: str, context=""):
 
     filtered = filter_menu(items, intent)
 
+    parsed_intent = intent
+
     ranked = rank_by_profit(filtered)
 
     candidates = ranked[:10]
 
     selected = llm_select(candidates, user_input, context)
 
+
     drink, dessert = get_upsells(items)
+
+    try:
+        save_ia(user_id, user_input, parsed_intent)
+    except Exception as e:
+        print(f"Erro ao salvar interação: {e}")
 
     return {
         "recommendations": selected,
@@ -29,3 +38,16 @@ def ai_suggest_menu(user_input: str, context=""):
             "dessert": dessert
         }
     }
+
+def save_ia(user_id, user_input, parsed_intent):
+    session = SessionLocal()
+    try:
+        new_ia = AIORM(
+            id_user=user_id,
+            input_text=user_input,
+            parsed_intent=parsed_intent
+        )
+        session.add(new_ia)
+        session.commit()
+    finally:
+        session.close()
