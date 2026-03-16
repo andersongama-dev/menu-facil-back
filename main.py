5e5fa9617afaa0e52eb695caabd60bfd9f9a615c
andersongama-dev <andersongamasilva5@gmail.com>
2026-03-15 21:02:24 -0300

    :construction: feat: extract_exclusions

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from starlette.middleware.cors import CORSMiddleware
+
 import models
 from fastapi import FastAPI
 from routes.userRoutes import router as user_routes
@@ -8,6 +10,20 @@ import uvicorn
 
 app = FastAPI()
 
+pp = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(user_routes)
 app.include_router(menu_routes)
 app.include_router(order_routes)
@@ -15,30 +31,3 @@ app.include_router(ai_suggest)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
-
-#class Item(BaseModel):
-#    text: str
-#    is_done: bool = False
-
-#pratos = []
-
-#@app.get("/")
-#def root():
-#    return {"Hello": "Word"}
-
-#@app.post("/pratos")
-#def create_prato(prato: Item):
-#    pratos.append(prato)
-#    return pratos
-
-#@app.get("/pratos", response_model=list[Item])
-#def list_prato(limit: int = 10):
-#    return pratos[0:limit]
-
-#@app.get("/pratos/{prato_id}", response_model=Item)
-#def get_prato(prato_id: int) -> Item:
-#    if prato_id < len(pratos):
-#        return pratos[prato_id]
-#    else:
-#        raise HTTPException(status_code=404, detail="Item not found")
-#
