3239daa39c1e664fcdf7c9906c55c623f692a402
andersongama-dev <andersongamasilva5@gmail.com>
2026-03-21 14:05:30 -0300

    :construction: feat: QR Code PIX

--- routes/orderRoutes.py
+++ routes/orderRoutes.py
@@ -16,14 +16,21 @@ class OrderOut(BaseModel):
     class Config:
         orm_mode = True
 
-
+class OrderWithQR(BaseModel):
+    order: OrderOut
+    qr_path: str
 
 router = APIRouter(prefix="/order", tags=["order"])
 
-@router.post("", response_model=OrderOut)
+@router.post("", response_model=OrderWithQR)
 def new_order(order: OrderCreate, current_user: str = Depends(get_current_user)):
     return serviceOrder.create_order(current_user, order.total_price)
 
 @router.get("/me", response_model=list[OrderOut])
 def all_orders(current_user: str = Depends(get_current_user)):
     return serviceOrder.find_order(current_user)
+
+@router.patch("/{order_id}/confirm", response_model=OrderOut)
+def confirm_order_payment(order_id: int, current_user: str = Depends(get_current_user)):
+    order = serviceOrder.confirm_payment_order(current_user, order_id)
+    return order
--- services/orderService.py
+++ services/orderService.py
@@ -1,7 +1,7 @@
 from database.connection import SessionLocal
 from models.order import OrderORM
 from utils.find_user import find_user
-
+from utils.genarete_qr_code import generate_qrcode_pix
 
 def create_order(user_email, price):
     session = SessionLocal()
@@ -14,10 +14,14 @@ def create_order(user_email, price):
         session.add(new_order)
         session.commit()
         session.refresh(new_order)
-        return new_order
+        qr_path = generate_qrcode_pix("andersongamasilva08@gmail.com", "Anderson Gama Silva", "SerraNegra",
+                                      price, tx_id="***", order_id=new_order.id_order)
+        return {
+            "order": new_order,
+            "qr_path": qr_path
+        }
     finally:
         session.close()
-
 
 def find_order(user_email):
     session = SessionLocal()
@@ -25,5 +29,26 @@ def find_order(user_email):
         user = find_user(user_email)
         orders = session.query(OrderORM).filter(OrderORM.id_user == user.id_user).all()
         return orders
+    finally:
+        session.close()
+
+def confirm_payment_order(user_email: str, order_id: int) -> OrderORM:
+    session = SessionLocal()
+    try:
+        user = find_user(user_email)
+
+        order = session.query(OrderORM).filter(
+            OrderORM.id_order == order_id,
+            OrderORM.id_user == user.id_user
+        ).one_or_none()
+
+        if not order:
+            raise ValueError(f"Ordem {order_id} não encontrada para o usuário {user_email}")
+
+        order.status = "paid"
+        session.commit()
+        session.refresh(order)
+        return order
+
     finally:
         session.close()
